5ccc5fd2cd4407fe7303c6972db8ed009dfc22d0
YCK1204 <https://github.com/YCK1204>
2024-08-20 02:27:35 -0400

    test | push test

--- main.c
+++ main.c
@@ -1,19 +1,27 @@
 #include <stdio.h>
+#include <string.h>
 
 extern size_t ft_strlen(const char *str);  // 어셈블리어 함수 선언
 extern char* ft_strcpy(char * dst, const char * src);
 extern int ft_strcmp(const char* s1, const char* s2);
+extern int ft_write(int fd, const char* buf, int size);
 
 int main() {
-    const char *text = "Hello, World!!!!!!!!!!!!!";
-    size_t length = ft_strlen(text);
+    const char *text = "Hello";
     char tmp[30];
-    ft_strcpy(tmp, text);
-    printf("Length of the string is: %zu\n", length);
-    printf("%s\n", tmp);
-    length = ft_strlen(tmp);
-    printf("Length of the string is: %zu\n", length);
-    printf("%d!!\n", ft_strcmp(tmp, text));
+
+    printf("string : %s\n", text);
+    printf("--------strlen--------\n");
+    printf("strlen : %d\n", strlen(text));
+    printf("ft_strlen : %d\n", ft_strlen(text));
+    printf("--------strcpy--------\n");
+    printf("strcpy : %s\n", strcpy(tmp, text));
+    printf("ft_strcpy : %s\n", ft_strcpy(tmp, text));
+    printf("--------strcmp--------\n");
+    ft_strcpy(tmp, "42");
+    printf("string1 : %s, string2 : %s\n", tmp, tmp);
+    printf("strcmp : %d\n", strcmp(tmp, tmp));
+    printf("ft_strcmp : %d\n", ft_strcmp(tmp, tmp));
     return 0;
 }
 
