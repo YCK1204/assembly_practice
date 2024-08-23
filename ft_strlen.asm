5b3ab8d377e00491a3b66e464dc0b9017b0f4991
YCK1204 <dpcks5959@naver.com>
2024-08-23 11:16:17 -0400

    fix | strlen ref rcx register

--- ft_strlen.asm
+++ ft_strlen.asm
@@ -2,18 +2,17 @@ section .text
     global ft_strlen
 
 ft_strlen:
-    xor rax, rax      ; rax를 0으로 초기화
-    xor rcx, rcx      ; rcx를 0으로 초기화 (문자 개수를 카운트할 변수)
+    xor rax, rax
+    xor rcx, rcx
 
 find_null_byte:
-    mov al, [rdi]     ; rdi가 가리키는 위치의 문자를 al에 로드
-    cmp al, 0         ; al의 값이 0(널 문자)인지 비교
-    je done           ; 널 문자가 발견되면 done 레이블로 점프
-    inc rdi           ; rdi를 증가시켜 다음 문자를 가리키도록 함
-    inc rcx           ; rcx를 증가시켜 카운트를 증가시킴
-    jmp find_null_byte; 루프를 계속함
+    mov al, [rdi + rcx]
+    cmp al, 0
+    je done
+    inc rcx
+    jmp find_null_byte
 
 done:
-    mov rax, rcx      ; rcx 값을 rax에 복사하여 반환 값으로 설정
+    mov rax, rcx
     ret
 
