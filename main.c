81a8c018935025fe7352ac20071e90bd1e31af06
YCK1204 <dpcks5959@naver.com>
2024-08-27 13:18:55 -0400

    modify | added bonus code

--- main.c
+++ main.c
@@ -3,6 +3,8 @@
 #include <unistd.h>
 #include <stdlib.h>
 
+typedef struct s_list { void *data; struct s_list *next; } t_list;
+
 extern size_t ft_strlen(const char *str);
 extern char* ft_strcpy(char * dst, const char * src);
 extern int ft_strcmp(const char* s1, const char* s2);
@@ -12,8 +14,16 @@ extern char* ft_strdup(const char* str);
 extern long ft_pow(long n, long cnt);
 extern long ft_atoi_base(const char* str, const char* base);
 extern long find_char(const char target, const char* str, long start);
+extern void ft_list_push_front(t_list **begin_list, void *data);
+extern int ft_list_size(t_list *begin_list);
+extern void ft_list_sort(t_list **begin_list, int (*cmp)());
+extern void ft_list_remove_if(t_list **begin_list, void *data_ref, int (*cmp)(), void (*free_fct)(void *));
+extern void ft_free_node(void* node);
+
+void leaks() { system("leaks libasm"); }
 
 int main() {
+    atexit(leaks);
     const char *text = "Hello";
     char tmp[30];
 
@@ -62,7 +72,54 @@ int main() {
 	int n = ft_atoi_base(str, base);
 	printf("ft_atoi_base : %d\n", n);
     }
+    printf("--------ft_list--------\n");
+    {
+    	t_list* lst = NULL;
 
+	ft_list_push_front(&lst, ft_strdup("3"));
+	ft_list_push_front(&lst, ft_strdup("1"));
+	ft_list_push_front(&lst, ft_strdup("5"));
+	ft_list_push_front(&lst, ft_strdup("3"));
+	ft_list_push_front(&lst, ft_strdup("3"));
+	ft_list_push_front(&lst, ft_strdup("3"));
+	ft_list_push_front(&lst, ft_strdup("8"));
+	ft_list_push_front(&lst, ft_strdup("7"));
+	ft_list_push_front(&lst, ft_strdup("3"));
+	ft_list_push_front(&lst, ft_strdup("9"));
+	ft_list_push_front(&lst, ft_strdup("3"));
+	if (lst == NULL)
+		printf("NULL\n");
+
+	printf("size : %d\n", ft_list_size(lst));
+
+	printf("---------------before sort----------------\n");
+	t_list* tmp = lst;
+	while (tmp != NULL) 
+	{
+		printf("%s\n", tmp->data);
+		tmp = tmp->next;
+	}
+
+	ft_list_sort(&lst, ft_strcmp);
+	printf("---------------after sort----------------\n");
+	tmp = lst;
+	while (tmp != NULL) 
+	{
+		printf("%s\n", tmp->data);
+		tmp = tmp->next;
+	}
+	printf("---------------remove node---------------\n");
+	printf("remove node %d\n", 3);
+	ft_list_remove_if(&lst, "3", ft_strcmp, ft_free_node);
+	tmp = lst;
+	while (tmp != NULL) 
+	{
+		printf("%s\n", tmp->data);
+		tmp = tmp->next;
+	}
+
+    }
+    
     return 0;
 }
 
